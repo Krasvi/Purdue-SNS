fad07c5ec4642e617d5d863b109bb0cef5fda146
John <jeong229@purdue.edu>
2025-05-21 09:58:33 +0900

    Add files via upload

---
+++ searchUserGUIService.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public interface searchUserGUIService {
+    public void createSearchUserGUI(String loggedInUsername, PrintWriter pw, BufferedReader bfr);
+
+    public void searchUsers();
+
+    public void showUserActions();
+}
